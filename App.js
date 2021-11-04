e83eeb5e6d333ac797d2478aa7e5e6d0f4f67842
Vibhu Singh <vibhusingh@brandeis.edu>
2021-11-04 15:41:33 -0400

    testing out the app

--- App.js
+++ App.js
@@ -3,19 +3,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return ( <View style = { styles.container } >
+        <Text> This is the </Text>
+        <StatusBar style = "auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
