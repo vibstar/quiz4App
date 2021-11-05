b6a32972fabc9f43224723318d52ca811680fc87
Vibhu Singh <vibhusingh@brandeis.edu>
2021-11-05 08:51:47 -0400

    fixed code formatting

--- App.js
+++ App.js
@@ -1,53 +1,89 @@
 import React, { Component, useState } from "react";
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, SectionList } from "react-native";
-
-const DATA = [
-  {
-    title: "Github Viewer",
-    data: []
-  },
-];
-
-const Item = ({ title }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
-//const [text, setText] = useState('');
+import { StatusBar } from "react"
+import { FlatList } from "react-native-gesture-handler";
 
 
 
 export default function App() {
-      const [userID, setUserID] = useState('');
-    return (<>
-        <SafeAreaView style={styles.topBox}>
-        <Text style = {styles.topText}>Github Viewer</Text>
-        </SafeAreaView>
+    const [userID, setUserID] = useState('');
+    const [repositories,getRepositories] = useState({
+        data: '',
+        loading: true
+    })
 
-        <SafeAreaView style={styles.gitInputId}>
-        <Text style = {styles.inputSection}>github ID:
-        
-        </Text>
+    
+
+    async function handleClick() {
+        const repositories = await axios.get('https://api.github.com/users/' + userID + '/repos');
+        getRepositories({
+            data: repositories,
+            loading: false
+        });
+
+    }
+    // const Item = ({ repositories }) => (
+    //     <View style={styles.greyBox}>
+    //       <Text style={styles.inputSection}>{repositories}</Text>
+    //     </View>
+    // );
+
+
+    const DATA = [
+        {
+          id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+          title: 'First Item',
+        },
+        {
+          id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+          title: 'Second Item',
+        },
+        {
+          id: '58694a0f-3da1-471f-bd96-145571e29d72',
+          title: 'Third Item',
+        },
+      ];
+      
+      const Item = ({ title }) => (
+        <View style={styles.greyBox}>
+          <Text style={styles.inputSection}>{title}</Text>
+        </View>
+      );
+      
+      const renderItem = ({ item }) => (
+        <Item title={item.title} />
+      );
+
+    return (
+    
+        <SafeAreaView style={styles.container}>
+
+        <Text style = {styles.topBox}>Github Viewer</Text>
+        <Text style = {styles.inputSection}>github ID:</Text>
         <TextInput 
         placeholder= 'userID'
         style={[{fontSize: 32}]}
         onChangeText={(val) => setUserID(val)}
         />
-        </SafeAreaView>
-
-        <SafeAreaView style={styles.repoButton}>
         <Button
-            onPress={() => { }}
-            title="Show repositories"
-        />
+            style = {styles.repoButton}
+            onClick= {handleClick}
+            title = 'show repositories'>
+        </Button>
+        <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        /> 
         </SafeAreaView>
 
-        </>
     );
   }
 
 const styles = StyleSheet.create({
+  container:{
+    flex: 1
+  },
   gitInputId: {
     flexDirection: 'row',
     backgroundColor: "#fff",
@@ -61,27 +97,28 @@ const styles = StyleSheet.create({
   },
   inputSection: {
     fontSize: 32,
+    borderWidth: 1,
     color: 'black'
   },
   topBox: {
-        paddingTop: '5%',
-        width: '100%',
-        height: '15%',
-        backgroundColor: '#000',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    topText : {
+    paddingTop: '5%',
+    width: '100%',
+    height: '15%',
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
     color: 'red',
     fontSize: 40,
     },
-    repoButton: {
-        alignItems: 'flex-start'
+  repoButton: {
+    alignItems: 'flex-start'
     },
-    showRepoButton: {
-        fontSize: 18,
-        color: '#3B39B2'
+  showRepoButton: {
+    fontSize: 18,
+    color: '#3B39B2'
+    },
+  greyBox: {
+    color: '#999999'
     }
-
 
 });
